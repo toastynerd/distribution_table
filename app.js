f81677c280a0a85ffd6f689ce574dc35e35f8d22
toastynerd <tylermorgan86@gmail.com>
2020-04-11 04:04:36 -0700

    cleanup and add distributor filter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var states = [];
 var allBrands = [];
+var allDist = [];
 $(function() {
   resultsTable = $('#results');
   var addRow = function(data) {
@@ -13,17 +14,21 @@ $(function() {
   }
 
   distTable.forEach(function(data) {
-    if (states.indexOf(data.state) == -1)
+    if (states.indexOf(data.state) === -1)
       states.push(data.state);
 
-    addRow(data);  
-
-    if (allBrands.indexOf(data.brand) == -1)
+    if (allBrands.indexOf(data.brand) === -1)
       allBrands.push(data.brand);
+
+    if (allDist.indexOf(data.distributor) === -1)
+      allDist.push(data.distributor);
+
+    addRow(data);  
   });
 
   var brandForm = $('#brand');
   var statesForm = $('#state');
+  var distForm = $('#distributor');
   states.forEach(function(state) {
     statesForm.append(`<option value="${state}">${state}</option>`);
   });
@@ -32,36 +37,23 @@ $(function() {
     brandForm.append(`<option value="${brand}">${brand}</option`);
   });
 
-  statesForm.on('change', function() {
-    resultsTable.children('tr').remove()
-    brandForm.children().not("option:first").remove();
-    var brands = [];
-    var state = statesForm.children("option:selected").val();
-
-    if (state === "all") {
-      allBrands.forEach(function(brand) {
-        brandForm.append(`<option value="${brand}">${brand}</option`);
-      });
-      distTable.forEach(function(data) {
-        addRow(data);
-      });
-    }
-    distTable.forEach(function(data) {
-      if (data.state == state) {
-        brandForm.append(`<option value="${data.brand}">${data.brand}</option>`);
-        addRow(data);
-      }
-    });
+  allDist.forEach(function(dist) {
+    distForm.append(`<option value="${dist}">${dist}</option>`);
   });
 
-  brandForm.on('change', function() {
+  var handleChange = function() {
     resultsTable.children('tr').remove();
-    state = statesForm.children("option:selected").val();
-    brand = brandForm.children("option:selected").val();
+    var state = statesForm.children("option:selected").val();
+    var brand = brandForm.children("option:selected").val();
+    var dist = distForm.children("option:selected").val();
+
     distTable.forEach(function(data) {
-      if ((data.state == state || state === "all") && (data.brand == brand || brand === "all")) {
+      if ((data.state === state || state === 'all') && (data.brand === brand || brand === 'all') && (data.distributor === dist || dist === 'all')) {
         addRow(data);
       }
     });
-  });
+  };
+  statesForm.on('change', handleChange);
+  brandForm.on('change', handleChange);
+  distForm.on('change', handleChange); 
 });
